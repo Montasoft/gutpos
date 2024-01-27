3f0cf1f046f4209aee19bf251c7060db70fd3001
JorgeDell <jorge.montagut@GMAIL.COM>
2024-01-27 17:07:00 -0500

    ajustado menu inicial

---
+++ GutPOS/settings/local.py
@@ -0,0 +1,67 @@
+from .base import *
+from decouple import config
+
+# para corregir error con mySQL
+import pymysql
+pymysql.install_as_MySQLdb()
+
+# decouple permite trabajar las variables de entorno
+# para recuperar:: EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = True
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+
+ALLOWED_HOSTS = ['*']
+# con ['*'] para recibir conexión de todos los orígnes
+# limitado es []
+# y ejecutar el servidor con "python manage.py runserver 0.0.0.0:8000"
+# permitirá conectarte desde el celular.
+# desde el movil o el pc ejecutar con la ip fisica del server 192.168.1.58:8000
+
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+  
+#Conectar al SQLite
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+# CONECTAR A MySQL
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'gutpos',
+            'USER': config('DBUSER'),
+            'PASSWORD': config('DBPASS'),
+            'HOST': 'localhost',
+            'PORT': '3360',
+        }
+    }
+"""
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# configuración de correo electrónico
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT = 587 #465
+EMAIL_HOST_USER = 'miniabastoslaroca@gmail.com'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+#EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
+#EMAIL_TIMEOUT =
+#EMAIL_SSL_KEYFILE= 
+#EMAIL_SSL_CERTFILE = 
+print(EMAIL_HOST_PASSWORD)
+
---
+++ GutPOS/urls.py
@@ -0,0 +1,57 @@
+"""GutPOS URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+
+import baseapp , compras, carrito, contacto, inventario
+
+from django.contrib.auth import views as auth_views
+
+#from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView
+#from django.contrib.auth.views import PasswordChangeDoneView,  passwpassord_reset, password_reset_done, password_reset_confirm, password_reset_complete
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+#    path('blog/', include('BlogApp.urls')),
+    path('contacto/', include('contacto.urls')),
+    path('auth/', include('autenticacion.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('tienda/', include('tienda.urls')),
+    path('carrito/', include('carrito.urls')),
+    path('pedidos/', include('pedidos.urls')),
+    path('compras/', include('compras.urls')),
+    path('ventas/', include('ventas.urls')),
+    path('', include('baseapp.urls')),
+ #   path(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+  #  path(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+   # path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #    auth_views.password_reset_confirm, name='password_reset_confirm'),
+ #   path(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    
+    path('password_reset', auth_views.PasswordResetView.as_view(template_name=   './registration/password_reset.html'), name='password_reset'),
+    path('password_reset/done', auth_views.PasswordResetDoneView.as_view(template_name='./registration/password_reset_done.html'),  name='password_reset_done'),  
+
+    path('password_change', auth_views.PasswordChangeView.as_view(template_name='./registration/login.html'),  name='password_change'),
+    path('password_change/done', auth_views.PasswordChangeDoneView.as_view(template_name='./registration/password_reset_done.html'),  name='password_change_done'),
+  #  path('password_reset', passReset.as_view(),  name='password_reset'),   
+  #  path('password_reset/done', PasswResetDoneView.as_view(), name='password_reset_done'), 
+ #   path('password_reset/done', PasswResetDoneView.as_view(template_name='registration/password_reset_done.html'),  name='password_reset_done'),  
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='./registration/password_reset_confirm.html'),  name='password_reset_confirm'),
+   # path('reset/done', PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),  name='password_reset_complete')
+
+]
+
